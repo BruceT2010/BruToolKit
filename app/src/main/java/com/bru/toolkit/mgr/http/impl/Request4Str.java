8457eb941c6c0e1ed57c55fc5035dac23de9f610
BruceDing <brucedingdev@foxmail.com>
2017-10-10 11:24:49 +0800

    project upload

---
+++ app/src/main/java/com/bru/toolkit/mgr/http/impl/Request4Str.java
@@ -0,0 +1,13 @@
+package com.bru.toolkit.mgr.http.impl;
+
+/**
+ * Class Desc: Class Desc
+ * <p/>
+ * Creator : BruceDing
+ * <p/>
+ * Email : brucedingdev@foxmail.com
+ * Create Time : 2016/8/15 18:53
+ */
+public interface Request4Str {
+    public void getStr(String strProfile);
+}
---
+++ app/src/main/java/com/bru/toolkit/pojo/BaseResponseBean.java
@@ -0,0 +1,13 @@
+package com.bru.toolkit.pojo;
+
+/**
+ * Class Desc: BaseResponseBean
+ * <p/>
+ * Creator : BruceDing
+ * <p/>
+ * Email : brucedingdev@foxmail.com
+ * <p/>
+ * Create Time : 2016/7/22 10:02
+ */
+public class BaseResponseBean {
+}
